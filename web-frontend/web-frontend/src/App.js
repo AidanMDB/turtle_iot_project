4c8ec5550de9a2e22f4fd0345bd39d7a6035b98f
Aidan D-B <adannhausenbrun@gmail.com>
2025-08-06 22:48:22 -0500

    created container for displaying historical data and most recent data collection together

--- web-frontend/web-frontend/src/App.js
+++ web-frontend/web-frontend/src/App.js
@@ -1,67 +1,104 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import { ResponsiveLine } from '@nivo/line';
+import { Line, ResponsiveLine } from '@nivo/line';
 import axios from 'axios'
 import React, { useState, useEffect } from 'react';
 
 
 
-const MyLineChart = ({ data }) => {
+const RecentReading = ({ data }) => {
+  if (!data || data.length === 0) return <p>No Data Available</p>
+
+  const latest = data[0];
+  const data_name = latest.id
+  const latest_data = latest.data[0]
+  const [key] = Object.keys(latest_data).filter(k => k !== 'x');
+  
+  const data_recent_value = latest_data[key]
+  
+
   return (
-  <ResponsiveLine
-    data={data}
-    margin={{ top: 100, right: 100, bottom: 200, left: 100 }}
-    xScale={{ type: 'point' }}
-    yScale={{ 
-      type: 'linear', 
-      stacked: false,
-      min: 'auto',
-      max: 'auto'
-    }}
-    axisBottom={{
-      orient: 'bottom',
-      legend: 'Time',
-      legendOffset: 36,
-      legendPosition: 'middle'
-    }}
-    axisLeft={{
-      orient: 'left',
-      legend: 'Temp F\u00B0',
-      legendOffset: '-40',
-      legendPosition: 'middle'
-    }}
-    colors={{ scheme: 'dark2' }}
-    pointSize={10}
-    pointBorderWidth={2}
-    pointLabelYOffset={2}
-    useMesh={true}
-    theme={{
-      // https://nivo.rocks/guides/theming/    for seeing everything in the themes
-      background:  "#282c34"
-    }}
-  />
+    <div className='recent-reading-container'>
+      <h3>{data_name}</h3>
+      <h1>{data_recent_value}{" \u00B0F"}</h1>
+    
+    </div>
+
+  );
+}
+
+
+const LineChart = ({ data }) => {
+  return (
+    <div className='line-graph-container'>
+      <ResponsiveLine
+        data={data}
+        margin={{ top: 20, right: 50, bottom: 50, left: 50 }}
+        xScale={{ type: 'point' }}
+        yScale={{ 
+          type: 'linear', 
+          stacked: false,
+          min: 'auto',
+          max: 'auto'
+        }}
+        axisBottom={{
+          orient: 'bottom',
+          legend: 'Time',
+          legendOffset: 36,
+          legendPosition: 'middle'
+        }}
+        axisLeft={{
+          orient: 'left',
+          legend: 'Temp F\u00B0',
+          legendOffset: '-40',
+          legendPosition: 'middle'
+        }}
+        colors={{ scheme: 'dark2' }}
+        pointSize={10}
+        pointBorderWidth={2}
+        pointLabelYOffset={2}
+        useMesh={true}
+        theme={{
+          // https://nivo.rocks/guides/theming/    for seeing everything in the themes
+          background:  "#282c34",
+          text: {
+            fontSize: 11,
+            fill: "#ffffffff"
+          },
+          
+        }}
+      />
+    </div>
+  );
+}
+
+
+
+const LineChartWithRecentValue = ({ data }) => {
+  return (
+    <div className="graph-recent-container">
+      <RecentReading data={data}/>
+      <LineChart data={data}/>
+    </div>
   )
 }
 
 
 
 
-
-
-
-function MyButton() {
+/* function MyButton() {
   return (
     <button>I'm a button</button>
   )
-}
+} */
 
 
 
-function App2() {
+function App() {
   const [chartData, setChartData] = useState([]);
 
   useEffect(() => {
-    axios.get("/mock-data.json")
+    axios.get("/mock-line-graph-data.json")
       .then((res) => {
         const rawData = res.data;
 
@@ -78,11 +115,13 @@ function App2() {
   .catch((err) => console.error("Error fetching data:", err));
   }, []);
 
+  console.log(chartData)
+
   return (
     <div className="App">
-      <h2>InfluxDB Sensor Data</h2>
+      <h2>Turtle IoT</h2>
       <div className='chart-container'></div>
-      <MyLineChart data={chartData} />
+      <LineChartWithRecentValue data={chartData} />
     </div>
   );
 }
@@ -91,7 +130,7 @@ function App2() {
 
 
 
-function App() {
+/* function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -111,6 +150,6 @@ function App() {
       </header>
     </div>
   );
-}
+} */
 
-export default App2;
+export default App;
